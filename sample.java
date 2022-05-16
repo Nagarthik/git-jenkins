da48c80e611a280d5e9d48c03f0f135447ceb970
Nagarthik <105629904+Nagarthik@users.noreply.github.com>
2022-05-16 12:58:17 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -3,7 +3,7 @@ public class HelloGuys {
   public static void main(String{} args) {
     System.out.println("Hey Naga");
     System.out.println("Hey Karthik");
-    
+    System.out.println("Hey Peter");
     
   }
 }
